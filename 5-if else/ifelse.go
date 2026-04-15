c3d5582424395ecc66fa9ca3070c942b907f2a66
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-04-15 11:38:13 +0530

    Add files via upload

---
+++ 5-if else/ifelse.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 11
+
+	// if age >= 18 {
+	// 	fmt.Println("person is adult")
+	// } else {
+	// 	fmt.Println("person is not an adult")
+	// }
+
+	// age:=11
+
+	// if age>=18{
+	// 	fmt.Println("person is  adult")
+	// } else if age>=12{
+	// 	fmt.Println("person is a teenager")
+	// } else  {
+	// 	fmt.Println("person is a kid")
+	// }
+
+	// var role= "admin"
+	// var hasPermissions=false
+
+	// if role== "admin" && hasPermissions{
+	// 	fmt.Println("yes")
+	// }
+     //we can declare a variable inside if construct
+
+	 if age:=20; age>=18{
+		fmt.Println("person is an adult",age)
+	 } else if age>=12{
+		fmt.Println("person is teenager",age)
+	 }
+
+}
+
