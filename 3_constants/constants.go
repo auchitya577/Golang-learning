8bf49f512c68b28e3105b84665a9b6b1165b4f3a
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-04-15 11:37:36 +0530

    Add files via upload

---
+++ 3_constants/constants.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const age = 40
+
+func main() {
+	// const name= "goltur"
+	//const age=30
+
+	//  const(
+	// 	host=3000
+	// 	post=2400
+	//  )
+
+	//  fmt.Println(host,post)
+	fmt.Println(age)
+}
