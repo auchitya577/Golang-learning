212f15fbc5c2253ea4af3d03b8bdef5b68b7a206
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-04-15 11:37:01 +0530

    Add files via upload

---
+++ 2_variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// //how to input variables
+	//  var name string= "goluuu"
+	//  fmt.Println(name)
+
+	//  //for bool
+
+	//  var isAdult bool= true
+	//  fmt.Println(isAdult)
+
+	//  //short hand syntax
+	// Name:= "golang"
+
+	// var price float32=10.5
+
+	price := 10.5
+	fmt.Println(price)
+
+}
