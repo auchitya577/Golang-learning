dc6a1faed0454582db10849641fab8d97d86d279
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-04-15 11:35:05 +0530

    Add files via upload

---
+++ 1_simple values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	//simple values
+	fmt.Println(1)
+
+	//string
+	fmt.Println("hello world")
+
+	//bool
+	fmt.Println(true)
+
+	//floats
+	fmt.Println(10.5/2)
+}
