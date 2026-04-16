5351f013379db4c29891c4d12cc2fb643e2ad3b5
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-04-16 19:51:46 +0530

    Add files via upload

---
+++ 7- arrays/arrays.go
@@ -0,0 +1,27 @@
+package main
+
+//numbered sequence of elements
+func main() {
+	// //ZEORED VALUES
+	//int=0, string="",bool= false
+
+	// // var nums[4]int
+	// // nums[0]=1
+	// // fmt.Println(nums)
+
+	// // var vals[4]bool
+	// // vals[1]=true
+	// // fmt.Println(vals)
+
+	// var name[3]string
+	// name[0]="golang"
+	// fmt.Println(name)
+
+	//to declare it in single line
+	// nums:=[5]int{1,2,3,4,5,}
+	// fmt.Println(nums)
+
+	// nums:=[2][2]int{{1,2} ,{3,4}}
+	// fmt.Println(nums)
+	// fixed size i.e. predictable
+}
