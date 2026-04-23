033d3ac3b05b7a9f9f6953be8391c9c3325ff929
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-04-23 10:40:11 +0530

    Add files via upload

---
+++ 10_maps/maps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//maps-> hash,object,dict
+	//creating map
+
+	m := make(map[string]string)
+
+	//SETTING AN ELEMENT
+	m["name"] = "golang"
+
+	fmt.Println(m["name"])
+
+}
